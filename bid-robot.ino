2a32abd6b4a496a1b10b989166a7c3ab676e4f98
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2019-02-01 13:01:00 +0100

    Adrian did some stuff that does not actually work, as usually.

--- bid-robot.ino
+++ bid-robot.ino
@@ -1,9 +1,16 @@
 //we know this is shit code. we are also a little sorry.
 
-const int LEFTLED = 3;
-const int RIGHTLED = 4;
-const int LEFTSENSOR = A0;
-const int RIGHTSENSOR = A1;
+const int IN1 = 10;
+const int IN2 = 9;
+const int IN3 = 8;
+const int IN4 = 7;
+const int ENL = 6;
+const int ENR = 5;
+const int UTBLACK = 20;
+const int UTGRAY = 70;
+
+const int LEFTSENSOR = A1;
+const int RIGHTSENSOR = A0;
 const int BOARDLED = 13;
 int DELTA = 50;
 int leftSensorBuffer[5];
@@ -16,13 +23,44 @@ int reversals = 0;
 bool findMode = true;
 int currentFindCounter = 0;
 int targetFindCounter = 0;
-int changesLeftUntilCenter = 2;
+int bordersUntilCenter = 2;
 
+class Category {
+  public:
+    Category(int, int, int);
+    int min;
+    int max;
+    int mean;
+    char color[1];
+} blackCategory, grayCategory, whiteCategory;
+
+Category::Category(int min = 1024, int max = 0, int mean = 0) {
+  this->min = min;
+  this->max = max;
+  this->mean = mean;
+}
+class Motor {
+public:
+  Motor();
+  int direction;
+  int state;
+  Category category;
+}
+
+Motor::Motor() {
+  this->direction = 1;
+  this->state = direction;
+  this->category = grayCategory;
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LEFTLED, OUTPUT);
-  pinMode(RIGHTLED, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENL, OUTPUT);
+  pinMode(ENR, OUTPUT);
   pinMode(BOARDLED, OUTPUT);
   pinMode(LEFTSENSOR, INPUT);
   pinMode(RIGHTSENSOR, INPUT);
@@ -30,61 +68,37 @@ void setup() {
 }
 
 void loop() {
-  /*
-  digitalWrite(LEFTLED, LOW);
-  digitalWrite(RIGHTLED, HIGH);
-  Serial.println("\nRight:");
-  Serial.println(String(analogRead(RIGHTSENSOR)));
-  Serial.println("\nLeft:");
-  Serial.println(String(analogRead(LEFTSENSOR)));
-  delay(500);
-  */
-  
-  if(!centerFound()){ 
-    while(!borderLog[0]){
-      findCircle();
-    }
-    delay(500);
-    
-    Serial.println("found circle");
+  if(!centerFound()){
     while(!centerFound()){
       alignWithSecant();
     }
-    Serial.println("located center");
     
     goToCenter();
     putFlag();
-    Serial.println("put flag");
   }
-  
-   // alignWithSecant();
-}
-
-void findCircle() {
-  alignWithSecant();
-  if(currentFindCounter == ((targetFindCounter / 2 + 1)*100)) {
-    turnNinetyDegrees();
-    currentFindCounter = 0;
-    targetFindCounter++;
-  }
-  else {
-    currentFindCounter++;
-  }  
-  delay(10);
 }
 
 void alignWithSecant(){
   getSensorData();
-  
   adjustCourse();
-  
-  displayMotorState();
+}
+
+void getSensorData(){
+  if(leftMotorState){
+    pushToBuffer(leftSensorBuffer, analogRead(LEFTSENSOR));
+  }
+  if(rightMotorState){
+    pushToBuffer(rightSensorBuffer, analogRead(RIGHTSENSOR));
+  }
 }
 
 void adjustCourse(){
   if(colorHasChanged(leftSensorBuffer) && colorHasChanged(rightSensorBuffer)){
     drive();
     logBorder(leftSensorBuffer);
+  }
+  else if(!colorHasChanged(leftSensorBuffer) && !colorHasChanged(rightSensorBuffer)) {
+    drive();
   }
   else if(colorHasChanged(leftSensorBuffer)) {
     if(rightMotorState == 0) {
@@ -104,22 +118,47 @@ void adjustCourse(){
       turnRight();
     }
   }
+  analogWrite(ENR, LOW);
+  analogWrite(ENL, LOW);
+  delay(200);
 }
 
 bool colorHasChanged(int valuesArray[5]){
+  return getCategory() != currentCategory;
+}
+
+Category getCategory(int valuesArray[5]){
   /*
   int difference = valuesArray[0] - valuesArray[1];
   if(abs(difference) > DELTA){
     return true;
   }
   */
-  return getCategory(valuesArray[0]) != getCategory(valuesArray[1]);
+  int min = 1024, max = 0, mean = 0;
+  for(int i = 0; i < 0; i++){
+    value = valuesArray[i];
+    if(value < min) min = value;
+    if(value > max) max = value;
+    mean += value;
+  }
+  mean = (int)((double) mean / 5.0)
+  newData = getCategory(mean);
+
+  if(mean < blackCategory.max) {
+    return blackCategory;
+  }
+  else if(mean < grayCategory.max) {
+    return grayCategory;
+  }
+  else {
+    return whiteCategory;
+  }
 }
 
 int getCategory(int value){
-  if(value < 60) return 1; //black
-  else if(value < 160) return 2; //grey
-  else if(value < 400) return 3; //white
+  if(value < ) return 1; //black
+  else if(value < UTGRAY) return 2; //grey
+  else return 3; //white
 }
 
 void logBorder(int anArray[5]){
@@ -131,16 +170,16 @@ void logBorder(int anArray[5]){
     pushToBuffer(borderLog, 2); //dark to light
   }
   if(centerFound()){
-    changesLeftUntilCenter--;
+    bordersUntilCenter--;
   }
   adjustDirection();
-  Serial.println("crossed border");
+  //Serial.println("crossed border");
 }
 
 void adjustDirection(){
   if(borderLog[0] == 1 && borderLog[1] == 1){
     reversals++;
-    Serial.println("U turn");
+    //Serial.println("U turn");
     turnAround();
   }
 }
@@ -152,46 +191,27 @@ void pushToBuffer(int anArray[5], int value){
   anArray[0] = value;
 }
 
-void displayMotorState(){
-  switch(leftMotorState) {
-    case 0 :  digitalWrite(LEFTLED, LOW);
-              break;
-    case 1 :  digitalWrite(LEFTLED, HIGH);
-              break;
-  }
-  switch(rightMotorState) {
-    case 0 :  digitalWrite(RIGHTLED, LOW);
-              break;
-    case 1 :  digitalWrite(RIGHTLED, HIGH);
-              break;
-  }
-  
-  delay(1000);
-}
-
-void turnNinetyDegrees() {
-  for(int i=0; i < 5; i++) {
-    digitalWrite(BOARDLED, HIGH);
-    delay(50);
-    digitalWrite(BOARDLED, LOW);
-    delay(50);
-  }
-  delay(200);
-}
-
 void turnAround() {
-  turnNinetyDegrees();
+  forward = -1;
+  if(leftMotorState) {
+    leftMotorState = forward;
+    startLeftMotor();
+  }
+  if(rightMotorState){
+    rightMotorState = forward;
+    startRightMotor();
+  }
 }
 
 
 void putFlag(){
-  digitalWrite(LEFTLED, LOW);
-  digitalWrite(RIGHTLED, LOW);
+  stopLeftMotor();
+  stopRightMotor();
   digitalWrite(BOARDLED, HIGH);
 }
 
 void goToCenter(){
-  while(changesLeftUntilCenter){
+  while(bordersUntilCenter){
     alignWithSecant();
   }
 }
@@ -202,31 +222,129 @@ bool centerFound(){
 
 void turnLeft(){
   rightMotorState = forward;
+  startRightMotor();
   leftMotorState = 0;
+  stopLeftMotor();
 }
 
 void turnRight(){
   rightMotorState = 0;
+  stopRightMotor();
   leftMotorState = forward;
+  startLeftMotor();
   Serial.println("turn right");
 }
 
 void drive(){
   rightMotorState = forward;
   leftMotorState = forward;
-  Serial.println("turn right");
+  if (forward == 1) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW); 
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);  
+  }
+  if (forward == -1) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);  
+  }
+  analogWrite(ENR, 100);
+  analogWrite(ENL, 120);
+  delay(20);
+  analogWrite(ENR, 80);
+  analogWrite(ENL, 100);
+  delay(20);
+  analogWrite(ENR, 60);
+  analogWrite(ENL, 80);
+  delay(20);
+  analogWrite(ENR, 40);
+  analogWrite(ENL, 60);
+  delay(20);
+  analogWrite(ENR, 20);
+  analogWrite(ENL, 40);
+  delay(20);
+  
+  Serial.println("drive");
+  
 }
 
-void getSensorData(){
-  if(leftMotorState){
-    pushToBuffer(leftSensorBuffer, analogRead(LEFTSENSOR));
-    //Serial.println("\nLEFT SENSOR: ");
-    //Serial.println(String(leftSensorBuffer[0]));
+void startLeftMotor(){
+  if (forward == 1) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);  
   }
-  if(rightMotorState){
-    pushToBuffer(rightSensorBuffer, analogRead(RIGHTSENSOR));
-    //Serial.println("\nRIGHT SENSOR: ");
-    //Serial.println(String(rightSensorBuffer[0]));
+  if (forward == -1) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);  
   }
-  //Serial.println("\n");
+  analogWrite(ENL, 140);
+  delay(20);
+  analogWrite(ENL, 120);
+  delay(20);
+  analogWrite(ENL, 100);
+  delay(20);
+  analogWrite(ENL, 80);
+  delay(20);
+  analogWrite(ENL, 60);
+  delay(20);
 }
+
+void stopLeftMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENL, 0);
+}
+
+void startRightMotor(){
+  if (forward == 1) {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);  
+  }
+  if (forward == -1) {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);  
+  }
+  analogWrite(ENR, 120);
+  delay(20);
+  analogWrite(ENR, 100);
+  delay(20);
+  analogWrite(ENR, 80);
+  delay(20);
+  analogWrite(ENR, 60);
+  delay(20);
+  analogWrite(ENR, 40);
+  delay(20);
+}
+
+void stopRightMotor() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENR, 0);
+}
+
+
+/* NOT USED
+
+void turnNinetyDegrees() {
+  startLeftMotor();
+  stopRightMotor();
+  delay(500);
+  startLeftMotor();
+  startRightMotor();
+}
+
+void findCircle() {
+  alignWithSecant();
+  if(currentFindCounter == ((targetFindCounter / 2 + 1)*100)) {
+    turnNinetyDegrees();
+    currentFindCounter = 0;
+    targetFindCounter++;
+  }
+  else {
+    currentFindCounter++;
+  }  
+  delay(10);
+}
+*/
